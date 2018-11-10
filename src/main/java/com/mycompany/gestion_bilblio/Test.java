374fb55050375500b404989b17de5a33f4e6fa5d
Dutervil <dwadson3@gmail.com>
2018-11-10 17:22:06 -0500

    La classe test mis a jour pour connection a la base de donnees

---
+++ src/main/java/com/mycompany/gestion_bilblio/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gestion_bilblio;
+
+/**
+ *
+ * @author Ing.Wadson
+ */
+public class Test {
+    public static void main(String []gg)
+    {
+        System.out.println("Yes");
+    }
+    
+}
