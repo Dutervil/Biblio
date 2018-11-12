19a092de38b81a3ac3a3ee9ecab856591feb40bf
Dutervil <dwadson3@gmail.com>
2018-11-12 09:34:15 -0500

    c'est la deuxieme classe de test

---
+++ src/main/java/com/mycompany/gestion_bilblio/test2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gestion_bilblio;
+
+/**
+ *
+ * @author Ing.Wadson
+ */
+public class test2 {
+     public static void main(String []gg)
+    {
+        System.out.println("c'est la deuxieme classe de test");
+    }
+}
